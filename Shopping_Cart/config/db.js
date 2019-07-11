820cfaa44b7c152361efd2e704c500d98665b5fd
JStheBest <zwang198@uiowa.edu>
2019-07-11 16:23:40 -0500

    shopping cart frontend

---
+++ Shopping_Cart/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("URI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("mongoDB Connected...");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
---
+++ Shopping_Cart/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+
+const app = express();
+const items = require("./routes/api/items");
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+//cnnect DB
+connectDB();
+
+// Use Routes
+app.use("/api/items", items);
+//app.use('/api/users', require('./routes/api/users'));
+//app.use('/api/auth', require('./routes/api/auth'));
+
+//port connect
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
