05efeb186c666d460391fe671a40477d4e7093aa
JStheBest <zwang198@uiowa.edu>
2019-07-15 20:26:17 -0500

    Connected fronted to backend

--- Shopping_Cart/client/src/components/AppNavbar.js
+++ Shopping_Cart/client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, Fragment } from 'react';
 import {
   Collapse,
   Navbar,
@@ -6,38 +6,80 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
-} from "reactstrap";
+  NavLink,
+  Container
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
+import Logout from './auth/Logout';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
+
+  toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
     });
-  }
+  };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className='navbar-text mr-3'>
+            <strong>{user ? `Welcome ${user.name}` : ''}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>
+      </Fragment>
+    );
+
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+        </NavItem>
+        <NavItem>
+          <LoginModal />
+        </NavItem>
+      </Fragment>
+    );
+
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Shopping Cart</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="https://github.com/willzhiwang/">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+        <Navbar color='dark' dark expand='sm' className='mb-5'>
+          <Container>
+            <NavbarBrand href='/'>ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                {isAuthenticated ? authLinks : guestLinks}
+              </Nav>
+            </Collapse>
+          </Container>
         </Navbar>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(AppNavbar);
--- Shopping_Cart/client/src/components/ShoppingList.js
+++ Shopping_Cart/client/src/components/ShoppingList.js
@@ -1,61 +1,45 @@
-import React, { Component } from "react";
-import { ListGroup, ListGroupItem, Container, Button } from "reactstrap";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import uuid from "uuid";
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-  state = {
-    items: [
-      {
-        id: uuid(),
-        name: "Water"
-      },
-      {
-        id: uuid(),
-        name: "Milk"
-      },
-      {
-        id: uuid(),
-        name: "Eggs"
-      }
-    ]
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
   };
+
+  componentDidMount() {
+    this.props.getItems();
+  }
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
+
   render() {
-    const { items } = this.state;
+    const { items } = this.props.item;
     return (
       <Container>
-        <br />
-        <Button
-          color="dark"
-          style={{ marginBottom: "2rem" }}
-          onClick={() => {
-            const name = prompt("Enter Item");
-            if (name) {
-              this.setState(state => ({
-                items: [...state.items, { id: uuid(), name }]
-              }));
-            }
-          }}
-        >
-          Add A Item
-        </Button>
         <ListGroup>
-          <TransitionGroup className="shopping-list">
-            {items.map(({ id, name }) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+          <TransitionGroup className='shopping-list'>
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames='fade'>
                 <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id !== id)
-                      }));
-                    }}
-                  >
-                    &times;
-                  </Button>
+                  {//this.props.isAuthenticated ? (
+                    <Button
+                      className='remove-btn'
+                      color='danger'
+                      size='sm'
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                    //) : null
+                  }
                   {name}
                 </ListGroupItem>
               </CSSTransition>
@@ -66,4 +50,13 @@ class ShoppingList extends Component {
     );
   }
 }
-export default ShoppingList;
+
+const mapStateToProps = state => ({
+  item: state.item,
+  //isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem }
+)(ShoppingList);
